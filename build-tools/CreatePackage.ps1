3946fe82cb2e3a8b74ae226771a65b3bed52fb9e
Mark van Aalst <markvanaalst@gmail.com>
2019-04-24 10:47:16 +0200

    Add project files.

---
+++ build-tools/CreatePackage.ps1
@@ -0,0 +1,19 @@
+Import-Module -Name SPE
+
+$session = New-ScriptSession -Username admin -Password b -ConnectionUri https://xm91u1.dev.local
+
+Invoke-RemoteScript -Session $session -ScriptBlock { 
+	# Create package
+	$package= New-Package -Name "SXA Styleguide"
+	
+	$package.Metadata.Author = "Mark van Aalst";
+	$package.Metadata.Publisher = "Sitecore Technical Marketing";
+	$package.Metadata.Version = Get-Date -Format FileDateTimeUniversal;
+
+
+	$configs = Get-UnicornConfiguration
+	$configs | New-UnicornItemSource -Project $package
+	Export-Package -Project $package -Path "C:\Projects\Sitecore.XA.Styleguide\export\Sitecore.XA.Styleguide.zip" -Zip
+}
+
+Stop-ScriptSession -Session $session
